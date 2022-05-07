87ebd43f44aafda0f9d50cc27e3a18008a4e600f
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2022-05-07 21:48:39 +0800

    imported and used CustomButton

--- snapshot-testing/App.js
+++ snapshot-testing/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { CustomButton } from './CustomButton.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CustomButton title="our button!"></CustomButton>
       <StatusBar style="auto" />
     </View>
   );
